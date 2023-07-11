fc95a82bd5c828bdcc5395c3e6a08ed942ca5635
chenkangkang1 <1779220244@qq.com>
2023-07-11 15:46:36 +0800

    注册ajax

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,4 +12,49 @@ $(function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
+
+  // 自定义校验规则
+  var form = layui.form
+  form.verify({
+    // 自定义了一个叫做 pwd 校验规则
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    // 校验两次密码是否一致的规则
+    repwd: function (value) {
+      // 通过形参拿到的是确认密码框中的内容
+      // 还需要拿到密码框中的内容
+      // 然后进行一次等于的判断
+      // 如果判断失败，则return 一个提示消息
+      var pwd = $('.reg-box [name=password]').val()
+      if (pwd !== value) {
+        return '两次密码不一致！'
+      }
+    },
+  })
+
+  // 发起注册用户的ajax请求
+  $('#form_reg').on('click', function (e) {
+    console.log('点击了注册完成按钮')
+    // 组织默认的提交行为
+    e.preventDefault()
+    // 发起ajax的post请求
+    var data = {
+      // username: $('#form_reg [name=username]').val(),
+      // password: $('#form_reg [name=password]').val(),
+      username: $('.zc-zh').val(),
+      password: $('.zc-pwd1').val(),
+    }
+    $.post('http://big-event-api-t.itheima.net/api/reguser', data, function (res) {
+      // console.log(res)
+      if (res.status !== 0) {
+        console.log('注册失败')
+        return layer.msg(res.message)
+      }
+      layer.msg('注册成功，请登录！')
+      console.log('成功了')
+
+      $('#link_login').click()
+    })
+  })
+
+  // 发起登录的Ajax请求
 })
